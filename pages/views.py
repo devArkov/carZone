abae0902c38f78333acffe4d8be2e25e7a7740e4
Alexandr Korobov <devarko@sapo.pt>
2023-01-17 22:06:56 +0300

    Cars, about, services, contact pages

--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,45 @@ from django.views.generic.base import TemplateView
 
 # Create your views here.
 class Home(TemplateView):
-    template_name = 'index.html'
+    template_name = 'pages/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         context['title'] = 'Home'
         return context
 
+
+class Cars(TemplateView):
+    template_name = 'pages/cars.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Cars'
+        return context
+
+
+class About(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'About'
+        return context
+
+
+class Services(TemplateView):
+    template_name = 'pages/services.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Services'
+        return context
+
+
+class Contact(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Contact'
+        return context
