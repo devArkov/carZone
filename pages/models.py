0abd6016f8477f475c7db6e83cd2bb917f00f360
Alexandr Korobov <devarko@sapo.pt>
2023-01-18 16:33:14 +0300

    Media settings

--- pages/models.py
+++ pages/models.py
@@ -6,11 +6,11 @@ class Team(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     designation = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo = models.ImageField(upload_to='media/photos/%Y/%m/%d')
     facebook_link = models.URLField(max_length=100)
     twitter_link = models.URLField(max_length=100)
     google_plus_link = models.URLField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.first_name) + str(self.last_name)
+        return str(self.first_name) + ' ' + str(self.last_name)
